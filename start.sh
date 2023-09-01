6f68bd4255d3778064fe6ddfae4c0a45f9a2b77f
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-09-01 00:47:42 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /app
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
